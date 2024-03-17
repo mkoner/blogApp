391b42a79666e3d1306c377344adda6f3113fc40
bennetstrauch <bennet.strauch@miu.edu>
2024-03-17 14:34:20 -0500

    comment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var ejs = require("ejs");
 const usersRouter = require('./routes/usersRoutes');
 const postsRouter = require('./routes/postsRoutes');
 const port = 80;
@@ -7,8 +8,12 @@ app.listen(port, () => {
     console.log('Your Server is running on port: ' +port);
 })
 
+//EJS Engine
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(usersRouter);
-app.use(postsRouter);
+// app.use(postsRouter);
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,73 @@
 const express = require('express');
 const path = require('path');
-const usersRouter = express.Router();
 
+const { error } = require('console');
+const options = {
+"caseSensitive": false,
+"strict": false
+};
+
+const usersRouter = express.Router(options);
+
+
+
+// USER STORAGE
+let users = [{email: 's@d', password: 's'}];
+
+// LOGIN
+usersRouter.get('/login', function (req, res, next) {
+    res.render('login',{failed:false});
+});
+
+usersRouter.post('/login', function (req, res, next) {
+    // #erneute formularbestätigung
+    let user = { email: req.body.email, password: req.body.password }
+
+    for (ele of users){
+        if(user.email == ele.email){
+            if(user.password == ele.password)
+                // #redirect
+                res.redirect('/');
+
+            // login failed (wrong password)
+            res.render('login', {failed:true});
+        }
+    }
+
+    // login failed (no user registered with email)
+    res.render('login', {failed:true});
+});
+
+
+// REGISTRATION
+usersRouter.get('/registration', function (req, res, next) {
+    res.render('registration',{emailUsed:false});
+});
+
+usersRouter.post('/registration', function (req, res, next) {
+    // #erneute formularbestätigung
+    let user = { email: req.body.email, password: req.body.password, username: req.body.username }
+
+    for (ele of users){
+        if(user.email == ele.email){
+
+            // registration failed - email already used
+            res.render('registration', {emailUsed:true});
+        }
+    }
+
+    users.push(user);
+    // #redirect
+    res.redirect('/');
+
+    // show all users 
+    console.log(users);
+});
 
 
 
 
 module.exports = usersRouter;
+
+
+
